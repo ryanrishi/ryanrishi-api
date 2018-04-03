d3877028446ea2545c20bc4b2122b59ea5e360c1
Ryan Rishi <ryan@ryanrishi.com>
2018-04-02 21:38:19 -0700

    add GET posts/:post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,13 +5,65 @@ const posts = [
   {
     id: 1,
     title: 'first post',
-    publishedAt: new Date()
+    publishedAt: new Date(),
+    content: 'first post content, isn\'t this interesting'
   }
 ];
 
+const serializePost = (post, single = true) => {
+  let serializedPost = {
+    type: 'posts',
+    id: post.id,
+    attributes: {
+      content: post.content,
+      'published-at': post.publishedAt,
+      title: post.title,
+    }
+  };
+  
+  if (single) {
+    return { data: serializedPost };
+  }
+  
+  return serializedPost;
+};
+
+const serializePosts = (posts) => {
+  return posts.map(post => serializePost(post, /* single */false));
+};
+
 /* GET posts listing. */
 router.get('/', function(req, res, next) {
-  res.json({ posts });
+  const serialized = serializePosts(posts);
+  
+  res.json({ data: serialized });
+});
+
+router
+.route('/:post')
+.get((req, res, next) => {
+  const id = parseInt(req.params.post);
+  
+  let post = posts.find(post => post.id === id);
+  
+  if (post) {
+    // const serialized = {
+    //   data: {
+    //     type: 'posts',
+    //     id: post.id,
+    //     attributes: {
+    //       title: post.title,
+    //       'published-at': post.publishedAt
+    //     }
+    //   }
+    // }
+    const serialized = serializePost(post);
+    res.json(serialized);
+  }
+  else {
+    res.status(404).json({ message: 'Not Found' });
+  }
 });
 
 module.exports = router;
+
